4c6e202cda30619cef85f3c1877e07bc0c2764dd
willianvasques-crypto <willian.santos.vasques@gmail.com>
2026-08-15 23:40:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const hierarchyData = [
     description: "Coordenação e supervisão",
     icon: "☫",
     members: [
-      { name: "Willian Vasques", role: "Coordenador do CAF" }
+      { name: "Monteiro", role: "Sub Cmd. Geral" }
     ]
   },
     {
@@ -24,7 +24,10 @@ const hierarchyData = [
     description: "Coordenação e supervisão",
     icon: "☫ |||||",
     members: [
-      { name: "Willian Vasques", role: "Coordenador do CAF" }
+      { name: "B. Lazz", role: "Cmd. SAE" },
+      { name: "Teixeira", role: "Cmd. SOP"},
+      { name: "Cayo", role: "Sub Cmd. SAE"},
+      { name: "Machado", role: "Sub Cmd. SOP"}
     ]
   },
     {
@@ -32,7 +35,11 @@ const hierarchyData = [
     description: "Coordenação e supervisão",
     icon: "☫ ||||",
     members: [
-      { name: "Willian Vasques", role: "Coordenador do CAF" }
+      { name: "Willian Vasques", role: "Coordenador do CAF" },
+      { name: "Augusto K.", role: "Cmd. Corregedoria"},
+      { name: "Willian F.", role: "Cmd. IOPE"},
+      { name: "Kauã N.", role: "Diretor da AFSU"},
+      { name: "George Wilson", role: "Cmd. IAMO"}
     ]
   },
   {
@@ -40,7 +47,8 @@ const hierarchyData = [
     description: "Coordenação e supervisão",
     icon: "☫ |||",
     members: [
-      { name: "Willian Vasques", role: "Coordenador do CAF" }
+      { name: "Samuel Souza", role: "Sub Cmd. IAMO" },
+      { name: "Bruno Escobar", role: "Sub Cmd. IOPE"}
     ]
   },
   {
@@ -48,8 +56,7 @@ const hierarchyData = [
     description: "Inspetoria",
     icon: "☫ ||",
     members: [
-      { name: "Exemplo 01", role: "Inspetor" },
-      { name: "Exemplo 02", role: "Inspetor" }
+      
     ]
   },
   {
@@ -57,15 +64,10 @@ const hierarchyData = [
     description: "Formação",
     icon: "△ |",
     members: [
-      { name: "Novo integrante", role: "Aluno" }
-    ]
-  },
-    {
-    rank: "Classe Especial",
-    description: "Formação",
-    icon: "❯❯❯❯",
-    members: [
-      { name: "Novo integrante", role: "Aluno" }
+      { name: "Jabson Tavares", role: "Sub Inspetor" },
+      { name: "Niel Novais", role: "Sub Inspetor"},
+      { name: "Wade Winston", role: "Sub Inspetor"},
+      { name: "*Arthur Almeida", role: "*Procurar Coord. do CAF"}
     ]
   },
    {
@@ -73,7 +75,17 @@ const hierarchyData = [
     description: "Formação",
     icon: "(❯❯❯",
     members: [
-      { name: "Novo integrante", role: "Aluno" }
+      { name: "João Victor", role: "Est. IOPE" },
+      { name: "César Gomez", role: "Membro do AFSU"}
+    ]
+  },
+ {
+    rank: "Classe Especial",
+    description: "Formação",
+    icon: "❯❯❯❯",
+    members: [
+      { name: "Adoni Tavares", role: "Membro AFSU & Coord. IOPE" },
+      { name: "Moreira Arruda", role: "Membro do CAF"}
     ]
   },
   {
@@ -81,7 +93,50 @@ const hierarchyData = [
     description: "Formação",
     icon: "|||",
     members: [
-      { name: "Novo integrante", role: "Aluno" }
+      { name: "Antonio Braga", role: "1ª Classe"},
+      { name: "Antonio Nunes", role: "1ª Classe"},
+      { name: "Arthur Ferraz", role: "1ª Classe"},
+      { name: "Bob Neto", role: "1ª Classe"},
+      { name: "Bruno Garcia", role: "1ª Classe"},
+      { name: "Cleiton Pereira", role: "1ª Classe"},
+      { name: "Diego Guerra", role: "1ª Classe"},
+      { name: "Emily Schimdt", role: "1ª Classe"},
+      { name: "Enzo Lima", role: "1ª Classe"},
+      { name: "Enzo Rodrigues", role: "1ª Classe"},
+      { name: "Gabriel B.", role: "1ª Classe"},
+      { name: "Gabriel Moraes", role: "1ª Classe"},
+      { name: "Gabriel Silveira", role: "1ª Classe"},
+      { name: "Gabriel Souza", role: "1ª Classe"},
+      { name: "Gael Silva", role: "1ª Classe"},
+      { name: "Gilson Fahur", role: "1ª Classe"},
+      { name: "Gustavo Carvalho", role: "1ª Classe"},
+      { name: "Hariel Denaro", role: "1ª Classe"},
+      { name: "Hudson Santos", role: "1ª Classe"},
+      { name: "Icaro Silva", role: "1ª Classe"},
+      { name: "James Mariano", role: "1ª Classe"},
+      { name: "João Junior", role: "1ª Classe"},
+      { name: "Josué Fernandes", role: "1ª Classe"},
+      { name: "Laura Nascimento", role: "1ª Classe"},
+      { name: "Leonardo Destro", role: "1ª Classe"},
+      { name: "Lucas Alves", role: "1ª Classe"},
+      { name: "Lucas Freitas", role: "1ª Classe"},
+      { name: "Marcos N.", role: "1ª Classe"},
+      { name: "Melo Brm", role: "1ª Classe"},
+      { name: "Monalisa Costa", role: "1ª Classe"},
+      { name: "Nando Escobar", role: "1ª Classe"},
+      { name: "Paulo Telhada", role: "1ª Classe"},
+      { name: "Pedro Henrique", role: "1ª Classe"},
+      { name: "Pedro Neumann", role: "1ª Classe"},
+      { name: "Reginaldo Lopes", role: "1ª Classe"},
+      { name: "Ricardo Muller", role: "1ª Classe"},
+      { name: "Roberto N.", role: "1ª Classe"},
+      { name: "Samuel Dias", role: "1ª Classe"},
+      { name: "Samuel Schimidt", role: "1ª Classe"},
+      { name: "Toni Santos", role: "1ª Classe"},
+      { name: "Vinicius Junior", role: "1ª Classe"},
+      { name: "Vitor Borges", role: "1ª Classe"},
+      { name: "Vitor santos", role: "1ª Classe"},
+      { name: "Willian Bone", role: "1ª Classe"}
     ]
   },
   {
@@ -89,7 +144,8 @@ const hierarchyData = [
     description: "Formação",
     icon: "||",
     members: [
-      { name: "Novo integrante", role: "Aluno" }
+      { name: "Ryan Cavalcante", role: "2ª Classe" },
+      { name: "Henrique Machado", role: "2ª Classe"}
     ]
   },
   {
@@ -97,16 +153,58 @@ const hierarchyData = [
     description: "Efetivo operacional",
     icon: "|",
     members: [
+      { name: "Carlos Alvez", role: "3ª Classe" },
+      { name: "Cauã Teixeira", role: "3ª Classe" },
+      { name: "Davi Perez", role: "3ª Classe" },
+      { name: "Gustavo Henrique", role: "3ª Classe" },
+      { name: "Hiago Vidotti", role: "3ª Classe" },
+      { name: "Joao Victor", role: "3ª Classe" },
+      { name: "Joao Vitor", role: "3ª Classe" },
+      { name: "Kevin Paiva", role: "3ª Classe" },
+      { name: "Leon Kennedy", role: "3ª Classe" },
+      { name: "Matheus Oliveira", role: "3ª Classe" },
       { name: "Pedro Alvarenga", role: "3ª Classe" },
-      { name: "Gustavo Henrique", role: "3ª Classe" }
+      { name: "Rafael Silva", role: "3ª Classe" },
+      { name: "Ramon Floresta", role: "3ª Classe" },
+      { name: "Rhyan Nascimento", role: "3ª Classe" },
+      { name: "Thiago Lima", role: "3ª Classe" },
+      { name: "Vidal Rodrigues", role: "3ª Classe" },
+      { name: "Vinicius Teixeira", role: "3ª Classe" }
+
     ]
   },
   {
     rank: "Aluno",
     description: "Formação",
-    icon: "🎓",
+    icon: "",
     members: [
-      { name: "Novo integrante", role: "Aluno" }
+      { name: "Bernardo Mesquita", role: "Aluno" },
+      { name: "Douglas Kovalsk", role: "Aluno" },
+      { name: "Eduardo Souza", role: "Aluno" },
+      { name: "Frankily Souza", role: "Aluno" },
+      { name: "Gabriel Ferreira", role: "Aluno" },
+      { name: "Guilherme Silva", role: "Aluno" },
+      { name: "Gustavo Felipe", role: "Aluno" },
+      { name: "Hugo Silva", role: "Aluno" },
+      { name: "João Silva", role: "Aluno" },
+      { name: "Luiz Andrade", role: "Aluno" },
+      { name: "Macedo Coimbra", role: "Aluno" },
+      { name: "Nascimento Lopes", role: "Aluno" },
+      { name: "Nino Abravanel", role: "Aluno" },
+      { name: "Rafael Siqueira", role: "Aluno" },
+      { name: "Sebastião V.", role: "Aluno" },
+      { name: "Felipe Souza", role: "Aluno" },
+      { name: "Gabriel Pires", role: "Aluno" },
+      { name: "Guilherme Const.", role: "Aluno" },
+      { name: "Gusmão Kovalsk", role: "Aluno" },
+      { name: "Henrique Souza", role: "Aluno" },
+      { name: "Kevin Valentino ", role: "Aluno" },
+      { name: "Luiz Henrique", role: "Aluno" },
+      { name: "Nycolas Novais", role: "Aluno" },
+      { name: "Vinicius Araújo", role: "Aluno" },
+      { name: "Vinicius Santos", role: "Aluno" },
+      { name: "Vinicius Santos", role: "Aluno" },
+      { name: "Vitor Mattos", role: "Aluno" }
     ]
   }
 ];
